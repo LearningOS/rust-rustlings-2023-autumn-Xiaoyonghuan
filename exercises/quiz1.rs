09ce0c0712f1e51cbe99c0f1c54430a6d811c5ce
sunhongchen <1468439019@qq.com>
2023-10-13 09:01:50 +0800

    完成至vecs1

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,18 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
 
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(num: u32) -> u32 {
+    let mut unit_price: u32; 
+    if num >40 {
+        unit_price=1
+        
+    }else {
+        unit_price=2
+    }
+    num*unit_price
+}
 
 // Don't modify this function!
 #[test]
